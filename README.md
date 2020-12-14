7726d6290109c5aa18dd1ef887dc667c6c3bd0dd
domsson <julien.d.dau@gmail.com>
2020-12-14 23:53:45 +0900

    Initial commit, everything is still super WIP, don't use yet, kthx

---
+++ README.md
@@ -0,0 +1,111 @@
+# nuru
+
+nuru is a file format specification for terminal drawings using Unicode 
+characters and, optionally, 4 or 8 bit colors via ANSI escape sequences.
+
+Additionally, a header-only C file is provided that provides some basic 
+data structures and functions for handling nuru files. 
+
+## Files
+
+Two file formats are being introduced;
+
+- `.nui`: nuru image file
+- `.nup`: nuru palette file
+
+## nuru image file (NUI)
+
+Binary file consisting of a 32 byte header and payload of varying length. 
+
+The payload is a list of image cells, in row-major order, that describes 
+the character, foreground and background color for that cell, plus optional 
+meta data.
+
+Fields larger than 1 byte are encoded using little endian byte order. 
+
+### NUI header (32 bytes)
+
+ - `00` - `06`: File signature `68 61 6b 6f` (`NURUIMG`) (7 bytes, char)
+ - `07`: File version (1 byte, uint8\_t)
+ - `08`: Glyph mode (1 byte, uint8\_t)
+ - `09`: Color mode (1 byte, uint8\_t)
+ - `10`: Meta data mode (1 byte, uint8\_t)
+ - `11` - `12`: Width (in character cells) (2 bytes, uint16\_t)
+ - `13` - `14`: Height (in character cells) (2 bytes, uint16\_t)
+ - `15`: default foreground color (1 byte, uint8\_t) 
+ - `16`: default background color (1 byte, uint8\_t)
+ - `17` - `23`: Name of the palette originally used to create the image (7 bytes, char) 
+ - `24` - `30`: Free to use (author signature) (7 bytes, char)
+ - `31`: Reserved (1 byte)
+ - `32`+: Cell data
+
+### NUI payload (cell data)
+
+    [[glyph] [color] [mdata]] [...]
+
+Where color is:
+
+    foreground-color background-color
+
+### Modes
+
+Glyph mode:
+
+ - `0`: 0 bits, print spaces only (can be used with background color)
+ - `1`: 8 bits, 256 characters as indices into a character palette\*
+ - `2`: 16 bits, directly using Unicode code points (only 2 byte code points, obviously)
+
+\*) If the palette is `default`, then the first 256 Unicode code points shall be used instead. 
+    This allows creation and distribution of files without the need for a palette file.
+
+Color mode:
+
+ - `0`: 0 bits (monochrome)
+ - `1`: 8 bits: 4 bits for foreground color, 4 bits for background color, see [4-bit ANSI color](https://en.wikipedia.org/wiki/ANSI_escape_code#3-bit_and_4-bit) (30 = 0, 31 = 1, ... 97 = 15)
+ - `2`: 16 bits: 8 bits for foreground color, 8 bits for background color, see [8-bit ANSI color](https://en.wikipedia.org/wiki/ANSI_escape_code#8-bit)
+
+Meta data mode:
+
+ - `0`: 0 bits (no meta data)
+ - `1`: 8 bits
+ - `2`: 16 bits
+
+### Default colors
+
+The default colors allow an application to make use of a terminal's default 
+foreground and/ or background color when displaying a cell. This is being done  
+by sacrificying two colors, one for each channel, to signify "no color". 
+
+In 8-bit color mode, colors `0` (`#000`, black) and `15` (`#fff`, white) are 
+recommended, as they both exist twice (`16` and `231`).
+
+ - `0`: default foreground color
+ - `1`: default background color 
+
+## nuru palette file (NUP)
+
+Binary file consisting of a 16 byte header and payload of fixed length. 
+
+The payload is simply a list of 2 byte Unicode code points that can be 
+used as the value of a `wchar_t` and therefore easily be printed.
+
+Fields larger than 1 byte are encoded using little endian byte order. 
+
+### NUP header
+
+ - `00` - `06`: `NURUPAL` (7 bytes, char)
+ - `07`: File format version as integer (1 byte, uint8\_t)
+ - `08`: Index of default fill character (usually space) (1 byte, uint8\_t)
+ - `09` - `15`: Name of the palette (7 bytes, char)
+ - `16` - `527`: 255 characters as 16 bit Unicode code points (512 bytes, uint16\_t)
+
+### NUP payload (characters)
+
+	codepoint0 ... codepoint255
+
+Each entry is a Unicode code point.
+
+## Related projects / file formats
+
+ - https://github.com/dylanaraps/pxltrm
+ - https://en.wikipedia.org/wiki/Netpbm#File_formats
---
+++ src/nuru.h
@@ -0,0 +1,227 @@
+#ifndef NURU_H
+#define NURU_H
+
+#include <stdio.h>      // size_t, fopen()
+#include <stdint.h>     // uint8_t, uint16_t
+#include <string.h>     // strcmp()
+
+// 
+// API
+// 
+
+#define NURU_IMG_SIGNATURE "NURUIMG"
+#define NURU_PAL_SIGNATURE "NURUPAL"
+
+typedef enum nuru_glyph_mode
+{
+	NURU_GLYPH_MODE_NONE    = 0,  // spaces only (needs a color mode)
+	NURU_GLYPH_MODE_PALETTE = 1,  // using a 255 char palette file
+	NURU_GLYPH_MODE_UNICODE = 2   // directly using 16 bit code points
+}
+nuru_glyph_mode_e;
+
+typedef enum nuru_color_mode
+{
+	NURU_COLOR_MODE_NONE = 0,     // no colors, monochrome
+	NURU_COLOR_MODE_4BIT = 1,     // 4-bit ANSI colors
+	NURU_COLOR_MODE_8BIT = 2      // 8-bit ANSI colors
+}
+nuru_color_mode_e;
+
+typedef enum nuru_mdata_mode
+{
+	NURU_MDATA_MODE_NONE  = 0,    // no meta data
+	NURU_MDATA_MODE_1BYTE = 1,    // 1 byte of meta data per cell
+	NURU_MDATA_MODE_2BYTE = 2     // 2 byte of meta data per cell
+}
+nuru_mdata_mode_e;
+
+typedef struct nuru_cell
+{
+	uint16_t ch;
+	uint8_t  fg;
+	uint8_t  bg;
+	uint16_t md;
+}
+nuru_cell_s;
+
+typedef struct nuru_img
+{
+	char     signature[8];
+	uint8_t  version;
+	uint8_t  glyph_mode;
+	uint8_t  color_mode;
+	uint8_t  mdata_mode;
+	uint16_t cols;
+	uint16_t rows;
+	uint8_t  fg;
+	uint8_t  bg;
+	char     palette[8];
+	char     comment[8];
+
+	nuru_cell_s *cells;
+	size_t num_cells;
+}
+nuru_img_s;
+
+typedef struct nuru_pal
+{
+	char    signature[8];
+	uint8_t version;
+	uint8_t space;
+	char    name[8];
+
+	uint16_t codepoints[256];
+}
+nuru_pal_s;
+
+int nuru_img_load(nuru_img_s *img, const char *file);
+int nuru_img_free(nuru_img_s *img);
+int nuru_pal_load(nuru_pal_s *pal, const char *file);
+
+nuru_cell_s* nuru_get_cell(nuru_img_s *img, uint16_t col, uint16_t row);
+
+// 
+// IMPLEMENTATION
+// 
+
+#ifdef NURU_IMPLEMENTATION
+
+int
+nuru_img_load(nuru_img_s *img, const char *file)
+{
+	FILE *fp;
+	fp = fopen(file, "rb");
+	if (fp == NULL)
+	{
+		return -1;
+	}
+
+	// header: signature
+	fread(&img->signature, 7, 1, fp);
+	img->signature[7] = 0;
+	if (strcmp(img->signature, NURU_IMG_SIGNATURE) != 0)
+	{
+		fclose(fp);
+		return -2;
+	}
+
+	// header: version
+	fread(&img->version, 1, 1, fp);
+
+	// header: modes
+	fread(&img->color_mode, 1, 1, fp);
+	fread(&img->glyph_mode, 1, 1, fp);
+	fread(&img->mdata_mode,  1, 1, fp);
+
+	// header: image size (TODO this assumes the system is little endian)
+	uint16_t buf = 0;
+	fread(&buf, 2, 1, fp);
+	img->cols = (0xFF00 & (buf << 8)) | (0x00FF & buf >> 8);
+	fread(&buf, 2, 1, fp);
+	img->rows = (0xFF00 & (buf << 8)) | (0x00FF & buf >> 8);
+
+	// header: default colors / key colors
+	fread(&img->fg, 1, 1, fp);
+	fread(&img->bg, 1, 1, fp);
+	
+	// header: palette name
+	fread(&img->palette, 7, 1, fp);
+	fread(&img->comment, 7, 1, fp);
+	img->palette[7] = 0;
+	img->comment[7] = 0;
+
+	// header: reserved byte
+	fread(&buf, 1, 1, fp);
+
+	// payload
+	img->num_cells = img->cols * img->rows;
+	img->cells = malloc(sizeof(nuru_cell_s) * img->num_cells);
+	if (img->cells == NULL)
+	{
+		fclose(fp);
+		return -3;
+	}
+
+	// TODO take different modes into account
+	for (size_t c = 0; c < img->num_cells; ++c)
+	{
+		uint8_t ch = 0;
+		fread(&ch, 1, 1, fp);
+		img->cells[c].ch |= 0x00FF & ch;
+		fread(&img->cells[c].fg, 1, 1, fp);
+		fread(&img->cells[c].bg, 1, 1, fp);
+	}
+
+	fclose(fp);
+	return img->num_cells;
+}
+
+nuru_cell_s*
+nuru_get_cell(nuru_img_s *img, uint16_t col, uint16_t row)
+{
+	size_t idx = (row * img->cols) + col;
+	if (idx >= img->num_cells)
+	{
+		return NULL;
+	}
+	return &img->cells[idx];
+}
+
+int
+nuru_img_free(nuru_img_s *img)
+{
+	if (!img)
+	{
+		return -1;
+	}
+	if (!img->cells)
+	{
+		return -1;
+	}
+
+	free(img->cells);
+	img->cells = NULL;
+	return 0;
+}
+
+int
+nuru_pal_load(nuru_pal_s *pal, const char *file)
+{
+	FILE *fp;
+	fp = fopen(file, "rb");
+	if (fp == NULL)
+	{
+		return -1;
+	}
+
+	// header: signature
+	fread(&pal->signature, 7, 1, fp);
+	pal->signature[7] = 0;
+	if (strcmp(pal->signature, NURU_PAL_SIGNATURE) != 0)
+	{
+		return -2;
+	}
+
+	// header: version
+	fread(&pal->version, 1, 1, fp);
+	
+	// header: default fill char (usually space)
+	fread(&pal->space, 1, 1, fp);
+
+	// header: palette name
+	fread(&pal->name, 7, 1, fp);
+	pal->name[7] = 0;
+
+	for (int i = 0; i < 256; ++i)
+	{
+		uint16_t buf = 0;
+		fread(&buf, 2, 1, fp);
+		pal->codepoints[i] = (0xFF00 & (buf << 8)) | (0x00FF & buf >> 8);
+	}
+
+	return 0;
+}
+
+#endif /* NURU_IMPLEMENTATION */
+#endif /* NURU_H */
